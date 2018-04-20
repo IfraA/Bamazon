ad4469ffd864309d4f33bbc82b0f9dac19a66048
IfraA <ifraa.aijaz@gmail.com>
2018-04-19 17:27:43 -0700

    final

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Bamazon
+Bamazon App
+
+This is an Amazon-like storefront App created using MySQL. The app takes in orders from customers and depletes stock from the store's inventory. It also notifies the User if the requested quantity of the product is not in stock.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,8 @@
 //pull in required packages
 var mysql = require('mysql');
-var inquire = require('inquirer');
+var inquirer = require("inquirer");
 var colors = require('colors');
-var Table = require('cli-table');
+// var Table = require('cli-table');
 //create connection to your database
 var connection = mysql.createConnection({
     host: "localhost",
@@ -42,70 +42,72 @@ function displayItems() {
 
 //prompt user purchase
 function promptUser() {
-    inquirer.prompt([{
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "item_id",
+                message: "Please enter the Item ID of the product you wish to purchase."
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "Please enter the quantity of the product.",
+                //validate input to verify the value entered is a number
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return true;
+                }
+            },
+        ]).then(function (input) {
+            console.log("Customer wants to purchase" + colors.red(input.quantity) + "of" + colors.yellow(input.item_id));
+            var item = input.item_id;
+            var quantity = input.quantity;
+            //query to confirm requested item exits in db
+            var queryStr = 'SELECT * FROM products WHERE ?';
+            connection.query(queryStr, { item_id: item }, function (err, data) {
+                if (err) throw err;
+                //if user selects invalid Item Id data will be empty
+                // console.log("data = " + JSON.stringify(data));
 
-        type: "input",
-        name: "item_id",
-        message: "Please enter the Item ID of the product you wish to purchase."
-    },
-    {
-        type: "input",
-        name: "quantity",
-        message: "Please enter the quantity of the product.",
-        //validate input to verify the value entered is a number
-        validate: function (value) {
-            if (NaN(value) === false) {
-                return true
-            };
-            return false;
-            console.log("Please enter a valid number");
-        },
-    },
-    ]).then(function (input) {
-        console.log("Customer wants to purchase" + input.quantity + "of" + input.item_id);
-        var item = input.item_id;
-        var quantity = input.quantity;
-        //query to confirm requested item exits in db
-        connection.query("SELECT * FROM products WHERE?", { item_id: item }, function (err, data) {
-            if (err) throw err;
-            console.log("data = " + JSON.stringify(data));
+                if (data.length === 0) {
+                    console.log("ERROR: Please choose a valid Item Id");
+                    displayItems();
+                }
+                else {
+                    var productData = data[0];
+                    // console.log('productData = ' + JSON.stringify(productData));
+                    // console.log('productData.stock_quantity = ' + productData.stock_quantity);
+                    // If the product is in Stock
+                    if (quantity <= productData.stock_quantity) {
+                        console.log(colors.blue('Congratulations, the product you requested is in stock! Placing order!'));
 
-            if (data.length == 0) {
-                console.log("ERROR: Please choose a valid Item Id");
-                displayItems();
-            }
-            else {
-                var productData = data[0];
-                console.log('productData = ' + JSON.stringify(productData));
-                console.log('productData.stock_quantity = ' + productData.stock_quantity);
-                // If the product is in Stock
-                if (quantity <= productData.stock_quantity) {
-                    console.log('Congratulations, the product you requested is in stock! Placing order!');
+                        // Construct the updating query string
+                        var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+                        // console.log('updateQueryStr = ' + updateQueryStr);
 
-                    // Construct the updating query string
-                    var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
-                    // console.log('updateQueryStr = ' + updateQueryStr);
+                        // Update the inventory
+                        connection.query(updateQueryStr, function (err, data) {
+                            if (err) throw err;
 
-                    // Update the inventory
-                    connection.query(updateQueryStr, function (err, data) {
-                        if (err) throw err;
+                            console.log('Your order has been placed! Your total is $' + colors.red(productData.price * quantity));
+                            console.log(colors.yellow('Thank you for shopping with us!'));
+                            console.log("\n---------------------------------------------------------------------\n");
 
-                        console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
-                        console.log('Thank you for shopping with us!');
+                            // End the database connection
+                            connection.end();
+                        })
+                    } else {
+                        console.log('Sorry, there is not enough product in stock, please check back later.');
                         console.log("\n---------------------------------------------------------------------\n");
 
-                        // End the database connection
-                        connection.end();
-                    })
-                } else {
-                    console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
-                    console.log('Please modify your order.');
-                    console.log("\n---------------------------------------------------------------------\n");
-
-                    displayInventory();
+                        displayInventory();
+                    }
                 }
-            }
-        })
-    })
+            })
+        });
 
 };
+
